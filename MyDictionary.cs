b2db48e1959a29a4f1615f4d345652fa3979c1f6
zeynepture <33553451+zeynepture@users.noreply.github.com>
2021-02-27 21:55:22 +0200

    Add files via upload

---
+++ MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T, Y>
+    {
+       public T[] forKeys;
+       public Y[] forVal;
+        
+        public MyDictionary()
+        {
+            forKeys = new T[0];
+            forVal = new Y[0];
+        }
+
+        public void Add(T key, Y val)
+        {
+            T[] tempKey = forKeys;
+            Y[] tempVal = forVal;
+            forKeys = new T[forKeys.Length + 1];
+            forVal = new Y[forVal.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                tempKey[i] = forKeys[i];
+            }
+            for (int i = 0; i < tempVal.Length; i++)
+            {
+                tempVal[i] = forVal[i];
+            }
+            forKeys[forKeys.Length - 1] = key;
+            forVal[forVal.Length - 1] = val;
+        
+        }
+        public int Count()
+        {
+            return forKeys.Length;
+        }
+    }
+   
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(15, "Zey");
+            myDictionary.Add(2, "Ali");
+
+            for (int i = 0; i < myDictionary.Count(); i++)
+            {
+                Console.WriteLine(myDictionary.forKeys[i] + " " + myDictionary.forVal[i]);
+            }
+          
+        }
+    }
+}
